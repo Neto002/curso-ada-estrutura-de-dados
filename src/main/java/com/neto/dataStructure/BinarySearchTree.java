4d240d46fe240f14fd981d9d5687600eb1a40553
Antonio Neto <antoniogfneto92@hotmail.com>
2024-06-12 23:24:29 -0300

    making BinarySearchTree generic

--- src/main/java/com/neto/dataStructure/BinarySearchTree.java
+++ src/main/java/com/neto/dataStructure/BinarySearchTree.java
@@ -1,15 +1,15 @@
 package com.neto.dataStructure;
 
-public class BinarySearchTree {
+public class BinarySearchTree<T extends Comparable<T>> {
 
-    public Node root;
+    public Node<T> root;
 
-    public class Node {
-        public int value;
-        public Node left;
-        public Node right;
+    public static class Node<T> {
+        public T value;
+        public Node<T> left;
+        public Node<T> right;
 
-        public Node(int value) {
+        public Node(T value) {
             this.value = value;
         }
     }
@@ -18,7 +18,7 @@ public class BinarySearchTree {
         inOrder(root);
     }
 
-    private void inOrder(final Node node) {
+    private void inOrder(final Node<T> node) {
         // E R D
         if (node == null) {
             return;
@@ -28,71 +28,75 @@ public class BinarySearchTree {
         inOrder(node.right);
     }
 
-    public void insert(int value) {
+    public void insert(T value) {
         if (root == null) {
-            root = new Node(value);
+            root = new Node<>(value);
         } else {
             insert(root, value);
         }
     }
 
-    private void insert(final Node node, final int value) {
+    private void insert(final Node<T> node, final T value) {
         if (node == null || value == root.value) {
             return;
         }
 
-        if (value < node.value) {
+//        if (value < node.value) {
+        if (value.compareTo(node.value) < 0 ) {
             if (node.left != null) {
                 insert(node.left, value);
             } else {
-                node.left = new Node(value);
+                node.left = new Node<T>(value);
             }
         } else {
             if (node.right != null) {
                 insert(node.right, value);
             } else {
-                node.right = new Node(value);
+                node.right = new Node<T>(value);
             }
         }
     }
 
-    public boolean contains(int value) {
+    public boolean contains(T value) {
         return contains(root, value);
     }
 
-    private boolean contains(final Node node, int value) {
+    private boolean contains(final Node<T> node, T value) {
         if (node == null) {
             return false;
         }
         if (node.value == value) {
             return true;
         }
-        if (value < node.value) {
+//        if (value < node.value) {
+        if (value.compareTo(node.value) < 0 ) {
             return contains(node.left, value);
         } else {
             return contains(node.right, value);
         }
     }
 
-    public int minValue(Node node) {
+    public T minValue(Node<T> node) {
         while (node.left != null) {
             node = node.left;
         }
         return node.value;
     }
 
-    public Node deleteNode(int value) {
+    public Node<T> deleteNode(T value) {
         return deleteNode(root, value);
     }
 
-    private Node deleteNode(final Node node, int value) {
+    private Node<T> deleteNode(final Node<T> node, T value) {
         if (root == null) {
             return null;
         }
 
-        if (value < node.value) {
+//        if (value < node.value) {
+        if (value.compareTo(node.value) < 0) {
             node.left = deleteNode(node.left, value);
-        } else if (value > node.value) {
+//        } else if (value > node.value) {
+        } else if (value.compareTo(node.value) > 0) {
             node.right = deleteNode(node.right, value);
         } else {
             if (node.left == null && node.right == null) {
@@ -104,7 +108,7 @@ public class BinarySearchTree {
             if (node.right == null) {
                 return node.left;
             }
-            int minValue = minValue(node.right);
+            T minValue = minValue(node.right);
             node.value = minValue;
             node.right = deleteNode(node.right, minValue);
         }
